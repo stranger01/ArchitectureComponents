f5ed7983809e2430556d1dd8861f034468384d2f
Stranger01 <joseantoniosysnet@gmail.com>
2020-07-20 10:02:00 -0500

    completed part #1

--- quizzApp/app/src/main/java/com/example/quizzapp/MainActivity.java
+++ quizzApp/app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quizzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startQuiz();
+
+            }
+        });
+    }
+
+    private void startQuiz() {
+
+        Intent intent = new Intent(MainActivity.this, QuizzActivity.class);
+        startActivity(intent);
+
     }
 }
