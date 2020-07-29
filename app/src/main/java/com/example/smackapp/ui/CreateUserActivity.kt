fabffba63be85a4f8eb4a4dab49aa2dd753127de
Stranger01 <joseantoniosysnet@gmail.com>
2020-07-29 10:56:37 -0500

    lesson-05

--- app/src/main/java/com/example/smackapp/ui/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackapp/ui/CreateUserActivity.kt
@@ -1,21 +1,65 @@
 package com.example.smackapp.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smackapp.R
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
+    /**
+     * Esta funcion genera un avatar
+     * de manera random en el registro
+     *
+     * */
     fun generateUserAvatarClicked(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        userAvatar = if (color == 0) {
+            "light$avatar"
+
+        } else {
+            "dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
 
     }
 
+    /**
+     * Esta funcion genera un color
+     * random en el registro
+     * el "bound" es el numero que esta excluido
+     *
+     * */
+
     fun generateColorClicked(view: View) {
+
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+
 
     }
 
