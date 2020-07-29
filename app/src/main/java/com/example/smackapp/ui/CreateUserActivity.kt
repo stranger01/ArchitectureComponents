3c62a74810cc17c6297aa69ebd1b9f3d6df234ae
Stranger01 <joseantoniosysnet@gmail.com>
2020-07-29 09:58:23 -0500

    lesson-04

---
+++ app/src/main/java/com/example/smackapp/ui/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.example.smackapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.smackapp.R
+
+class CreateUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatarClicked(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+
+    fun createUserClicked() {
+
+    }
+}
--- app/src/main/java/com/example/smackapp/ui/LoginActivity.kt
+++ app/src/main/java/com/example/smackapp/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smackapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,7 +16,11 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    fun loginCreateUserBtnClicked(view: View) {
+    fun loginCreateUserClicked(view: View) {
+
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+
 
     }
 
