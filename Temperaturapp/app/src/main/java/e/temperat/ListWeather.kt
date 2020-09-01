9e8fa088b04cf107ae7d889b233cfca0356247f7
Stranger01 <joseantoniosysnet@gmail.com>
2020-09-01 09:10:24 -0500

    updated the API and now running under nullable types!

--- Temperaturapp/app/src/main/java/e/temperat/ListWeather.kt
+++ Temperaturapp/app/src/main/java/e/temperat/ListWeather.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListWeather : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +17,45 @@ class ListWeather : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.foreCastListView)
 
         val artistas = listOf(
-            "Romeo Santos", "Hot Chili", "Jaguares", "Caifanes", "Cafe tacuba",
-            "Mana", "Farruko", "Don omar", "Los toros van", "Ozone", "Marylin Manson", "Audioslave", "Linkin Park")
+            "Romeo Santos",
+            "Hot Chili",
+            "Jaguares",
+            "Caifanes",
+            "Cafe tacuba",
+            "Mana",
+            "Farruko",
+            "Don omar",
+            "Los toros van",
+            "Ozone",
+            "Marylin Manson",
+            "Audioslave",
+            "Linkin Park"
+        )
 
         val adapter =
             ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, artistas)
         listView.adapter = adapter
+
+
+        var retriever = WeatherRetriever()
+
+        val callback = object : Callback<List<Query>> {
+            override fun onResponse(
+                call: Call<List<Query>>,
+                response: Response<List<Query>>
+            ) {
+                Toast.makeText(applicationContext,"Suceed:",Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onFailure(call: Call<List<Query>>, t: Throwable) {
+
+                Toast.makeText(applicationContext,"Failed",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        retriever.getForecast(callback)
 
     }
 }
--- Temperaturapp/app/src/main/java/e/temperat/WeatherHelp.kt
+++ Temperaturapp/app/src/main/java/e/temperat/WeatherHelp.kt
@@ -1,11 +1,29 @@
 package e.temperat
 
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface WeatherApi {
-    @GET("http://api.openweathermap.org/")
-    fun getForecast(): Call<List<Forecast>>
+    @GET("?location=sunnyvale,ca&format=json")
+    fun getForecast(): Call<List<Query>>
 }
 
-class Forecast(val high: String, val low: String)
+class Query()
+
+class WeatherRetriever {
+    val service: WeatherApi
+
+    init {
+        val retrofit = Retrofit.Builder().baseUrl("https://weather-ydn-yql.media.yahoo.com/forecastrss/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+       service = retrofit.create(WeatherApi::class.java)
+    }
+
+    fun getForecast(callback : Callback<List<Query>>){
+        val call = service.getForecast()
+        call.enqueue(callback)
+    }
+}
