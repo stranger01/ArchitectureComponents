df320f87d1d96e96761c3ad0280af9644117f1b0
Stranger01 <joseantoniosysnet@gmail.com>
2020-09-11 09:16:33 -0500

    added Todo

--- FuckingDo/app/src/main/java/e/fuckingdo/MainActivity.kt
+++ FuckingDo/app/src/main/java/e/fuckingdo/MainActivity.kt
@@ -1,11 +1,13 @@
 package e.fuckingdo
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val intent = Intent(applicationContext, AddToDoActivity::class.java)
+            startActivity(intent)
         }
     }
 
