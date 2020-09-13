5e171db221b4afad34e293c7c53606ff12b878f7
Stranger01 <joseantoniosysnet@gmail.com>
2020-09-13 12:47:54 -0500

    realm

--- FuckingDo/app/src/main/java/e/fuckingdo/AddToDoActivity.kt
+++ FuckingDo/app/src/main/java/e/fuckingdo/AddToDoActivity.kt
@@ -7,5 +7,7 @@ class AddToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_to_do)
+
+        title = "Add a new note"
     }
 }
