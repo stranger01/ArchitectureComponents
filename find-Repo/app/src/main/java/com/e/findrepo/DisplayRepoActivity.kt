d17d0ff8bd2efdc3eee36c21ee0f749d69c5d58e
Stranger01 <joseantoniosysnet@gmail.com>
2020-09-16 10:12:08 -0500

    custom list view rows for Github repo finder

--- find-Repo/app/src/main/java/com/e/findrepo/DisplayRepoActivity.kt
+++ find-Repo/app/src/main/java/com/e/findrepo/DisplayRepoActivity.kt
@@ -3,6 +3,9 @@ package com.e.findrepo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DisplayRepoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,31 @@ class DisplayRepoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_repo)
 
         val searchTerm = intent.getStringExtra("searchTerm")
+
+        val callback = object : Callback<GithubSearchResult> {
+            override fun onResponse(
+                call: Call<GithubSearchResult>,
+                response: Response<GithubSearchResult>
+            ) {
+
+                val searchResult = response.body()
+                if (searchResult != null) {
+                    for (repo in searchResult!!.items)
+                        //Toast.makeText(applicationContext, "funciona $repo", Toast.LENGTH_SHORT).show()
+                        println(repo)
+                }
+            }
+
+            override fun onFailure(call: Call<GithubSearchResult>, t: Throwable) {
+
+                Toast.makeText(applicationContext, "No", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        val retriever = GithubRetriever()
+        if (searchTerm != null) {
+            retriever.getRepo(callback, searchTerm)
+        }
 
         Toast.makeText(applicationContext, "La busqueda es $searchTerm", Toast.LENGTH_SHORT).show()
     }
