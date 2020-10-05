3d5bdf68682c5b220f8ed8a55853bb5e57b73845
Stranger01 <joseantoniosysnet@gmail.com>
2020-10-05 11:34:29 -0500

    activity state

---
+++ PluralSightKotlin/app/src/main/java/com/jwhh/notekeeper/ItemsActivity.kt
@@ -0,0 +1,168 @@
+package com.jwhh.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_items.*
+import kotlinx.android.synthetic.main.app_bar_items.*
+import kotlinx.android.synthetic.main.content_items.*
+
+class ItemsActivity : AppCompatActivity(),
+        NavigationView.OnNavigationItemSelectedListener,
+        NoteRecyclerAdapter.OnNoteSelectedListener {
+
+
+
+    private val noteLayoutManager by lazy {
+        LinearLayoutManager(this)
+    }
+    private val noteRecyclerAdapter by lazy {
+        val adapter = NoteRecyclerAdapter(this, DataManager.loadNotes())
+        adapter.setOnSelectedListener(this)
+        adapter
+    }
+
+    private val courseLayoutManager by lazy {
+        GridLayoutManager(this, 2)
+    }
+    private val courseRecyclerAdapter by lazy {
+        CourseRecyclerAdapter(this, DataManager.courses.values.toList())
+    }
+
+    private val recentlyViewedNoteRecyclerAdapter by lazy {
+        val adapter = NoteRecyclerAdapter(this, viewModel.recentlyViewedNotes)
+        adapter.setOnSelectedListener(this)
+        adapter
+    }
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this)[ItemsActivityViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_items)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            startActivity(Intent(this, NoteActivity::class.java))
+
+        }
+
+        handleDisplaySelection(viewModel.navDrawerDisplaySelection)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun displayNotes() {
+        listItems.layoutManager = noteLayoutManager
+        listItems.adapter = noteRecyclerAdapter
+
+        nav_view.menu.findItem(R.id.nav_notes).isChecked = true
+    }
+
+    private fun displayCourses() {
+        listItems.layoutManager = courseLayoutManager
+        listItems.adapter = courseRecyclerAdapter
+
+        nav_view.menu.findItem(R.id.nav_courses).isChecked = true
+    }
+
+    private fun displayRecentlyViewedNotes() {
+        listItems.layoutManager = noteLayoutManager
+        listItems.adapter = recentlyViewedNoteRecyclerAdapter
+
+        nav_view.menu.findItem(R.id.nav_recent_notes).isChecked = true
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        listItems.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.items, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_notes,
+            R.id.nav_courses,
+            R.id.nav_recent_notes -> {
+                handleDisplaySelection(item.itemId)
+                viewModel.navDrawerDisplaySelection = item.itemId
+            }
+            R.id.nav_share -> {
+                handleSelection("Don't you think you've shared enough")
+            }
+            R.id.nav_send -> {
+                handleSelection("Send")
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun handleDisplaySelection(itemId: Int) {
+        when (itemId) {
+            R.id.nav_notes -> {
+                displayNotes()
+            }
+            R.id.nav_courses -> {
+                displayCourses()
+            }
+            R.id.nav_recent_notes -> {
+                displayRecentlyViewedNotes()
+            }
+        }
+
+    }
+
+
+
+    override fun onNoteSelected(note: NoteInfo) {
+       viewModel.addToRecentlyViewedNotes(note)
+    }
+
+
+    private fun handleSelection(message: String) {
+        Snackbar.make(listItems, message, Snackbar.LENGTH_LONG).show()
+    }
+}
---
+++ PluralSightKotlin/app/src/main/java/com/jwhh/notekeeper/ItemsActivityViewModel.kt
@@ -0,0 +1,33 @@
+package com.jwhh.notekeeper
+
+import androidx.lifecycle.ViewModel
+
+class ItemsActivityViewModel : ViewModel() {
+
+    var navDrawerDisplaySelection = R.id.nav_notes
+
+    private val maxRecentlyViewedNotes = 5
+    val recentlyViewedNotes = ArrayList<NoteInfo>(maxRecentlyViewedNotes)
+
+
+    fun addToRecentlyViewedNotes(note: NoteInfo) {
+        // Check if selection is already in the list
+        val existingIndex = recentlyViewedNotes.indexOf(note)
+        if (existingIndex == -1) {
+            // it isn't in the list...
+            // Add new one to beginning of list and remove any beyond max we want to keep
+            recentlyViewedNotes.add(0, note)
+            for (index in recentlyViewedNotes.lastIndex downTo maxRecentlyViewedNotes)
+                recentlyViewedNotes.removeAt(index)
+        } else {
+            // it is in the list...
+            // Shift the ones above down the list and make it first member of the list
+            for (index in (existingIndex - 1) downTo 0)
+                recentlyViewedNotes[index + 1] = recentlyViewedNotes[index]
+            recentlyViewedNotes[0] = note
+        }
+
+    }
+
+
+}
